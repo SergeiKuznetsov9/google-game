1c34f5d0cd7e67635d6ec6d284d2856de1d4d31c
Sergey Kuznetsov <SKuznetsov3@alfabank.ru>
2024-07-26 08:26:28 +0300

    отчистка

--- ui/components/App.component.js
+++ ui/components/App.component.js
@@ -2,9 +2,6 @@ import { GridComponent } from "./Grid/Grid.component.js";
 import { ResultPanelComponent } from "./ResultPanel/ResultPanel.component.js";
 import { SettingsComponent } from "./Settings/Settings.component.js";
 
-// Так уже лучше, но тоже не хорошо, т.к. этот компонент призван быть нейким агрегатором
-// всего приложения, т.е. склеивать воедино более мелкие крипичики. Реализуем:
-
 export const AppComponent = () => {
   const element = document.createElement("div");
 
