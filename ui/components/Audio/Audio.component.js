4edef37183375b1c0092dc638d0845b91fc53278
Sergey Kuznetsov <Kuznetsoff9@yandex.ru>
2024-08-07 15:07:18 +0300

    пофиксить звук

--- ui/components/Audio/Audio.component.js
+++ ui/components/Audio/Audio.component.js
@@ -2,8 +2,8 @@ import { EVENTS } from "../../../constants/constants.js";
 import { subscribe } from "../../../core/state-manager.js";
 
 export const AudioComponent = () => {
-  const catchAudio = new Audio("assets/sounds/catch.wav");
-  const missAudio = new Audio("assets/sounds/miss.mp3");
+  const catchAudio = new Audio("ui/assets/sounds/catch.wav");
+  const missAudio = new Audio("ui/assets/sounds/miss.mp3");
 
   subscribe((event) => {
     if (event.name === EVENTS.GOOGLE_RAN_AWAY && event.payload) {
